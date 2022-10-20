a6d8a18659c1218daed1dbce9edf8bc8e233d22f
vkataria0364 <vkataria0364@conestogac.on.ca>
2022-10-20 17:12:33 -0400

    Committing both projects

---
+++ NUnitDemonstration/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace NUnitDemonstration
+{
+//Vishant Kumar Kataria - Code
+    public class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
---
+++ NUnitDemonstrationInner/TriangleTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnitDemonstration;
+
+
+namespace NUnitDemonstration
+{
+    public class TriangleTest
+    {
+        [TestFixture]
+        public class TriangleTest1
+//Vishant Kumar Kataria Test
+        {
+
+            [Test]
+
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 50;
+                int secondAngle = 70;
+                int thirdAngle = 60;
+
+                string expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                Assert.AreEqual(expected, actual);
+
+            }
+        }
+       
+    }
+}
